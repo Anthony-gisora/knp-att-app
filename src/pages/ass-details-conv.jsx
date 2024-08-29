b2f8cd7f484c4a65f6ea927b82c028a20297fabe
Tonny_G <gesoranthony@gmail.com>
2024-08-29 12:51:19 +0300

    removed unnecessery files

---
+++ src/pages/ass-details-conv.jsx
@@ -0,0 +1,9 @@
+const AssDetailsConv = () => {
+  return (
+    <div className="h-full bg-gray-800">
+      <h1>Hello, let's build it...!</h1>
+    </div>
+  );
+};
+
+export default AssDetailsConv;
